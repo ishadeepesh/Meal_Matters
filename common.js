df97026a88b4a9926448356de23423d1837e184e
J ATULYA <109218299+jatulya@users.noreply.github.com>
2023-11-23 09:54:05 +0530

    created and updated common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 const myElement = document.getElementById("email1");
     function myEventHandler() {
-
+      console.log("From common.js -> the one that handles email")
       var userEmail = document.getElementById("email1").value;
 
       localStorage.setItem("userEmail", userEmail);
